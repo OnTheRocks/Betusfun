f192f0d5b2e065b8203c97ca76cd565d97272a98
Nathan <nathan_huber@yahoo.com>
2021-09-15 00:44:29 -0500

    Setting up componets and stylesheet.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return(
+    <div>
+      <h3 className="Division">AFC East</h3>
+      <h1 className="TeamName">Miami Dolphins</h1>
+    </div>
+  )
+}
+
+export default Header;
