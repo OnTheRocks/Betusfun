624e64e2b983fa3cd5f3a743c7bb39dfc5641434
Nathan <nathan_huber@yahoo.com>
2021-09-27 23:59:27 -0500

    Added Schedule.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Schedule from "./components/Schedule";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Schedule />
     </div>
   );
 }
---
+++ src/components/Schedule.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Schedule = () => {
+  return(
+    <div>
+      <div className="scheduleMain">2021 Schedule</div>
+    </div>
+  )
+}
+
+export default Schedule;
