6a602a6c95f0a8f2355e1d7d1feeb5b2b45faeb3
OnTheRocks <nathan_huber@yahoo.com>
2021-11-24 01:31:35 -0600

    Working on setting up

---
+++ src/components/Table.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Table() {
+  return (
+    <div>
+
+      <table>
+      	<thead>
+      		<tr>
+      			<th>OFFENCE</th>
+      			<th>AVG.</th>
+      			<th>RANK</th>
+      		</tr>
+      	</thead>
+      	<tbody>
+      		<tr>
+      			<td>Points Scored</td>
+      			<td>25,3</td>
+      			<td>15</td>
+      		</tr>
+      		<tr>
+      			<td>Total yds per Play</td>
+      			<td>5,3</td>
+      			<td>22</td>
+      		</tr>
+      		<tr>
+      			<td>Total yds per Game</td>
+      			<td>339,0</td>
+      			<td>22</td>
+      		</tr>
+      		<tr>
+      			<td>Rush yds per Att</td>
+      			<td>3,9</td>
+      			<td>29</td>
+      		</tr>
+      		<tr>
+      			<td>Rush yds per Game</td>
+      			<td>105,5</td>
+      			<td>22</td>
+      		</tr>
+      		<tr>
+      			<td>Pass yds per Att</td>
+      			<td>6,7</td>
+      			<td>21</td>
+      		</tr>
+      		<tr>
+      			<td>Pass yds per Game</td>
+      			<td>233,5</td>
+      			<td>20</td>
+      		</tr>
+      	</tbody>
+      </table>
+      
+    </div>
+  )
+}
---
+++ src/components/TeamPage/Team.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./Team.css"
+
+export default function Team() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
